e3c22d61f56e0d8a7c2c4bc296ddfeb2a79e3ade
Sergio Pavanelli <sergiopavanelli@MacBook-Pro-de-Sergio.local>
2024-04-09 15:57:23 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String nome_aluno1 = "João";
+        String ra_aluno1 = "123456";
+        
+        int nota1_aluno1 = 25;
+        int nota2_aluno1 = 30;  
+        int nota3_aluno1 = 35;
+
+        int tota_notas_aluno1 = nota1_aluno1 + nota2_aluno1 + nota3_aluno1;
+
+        String nome_aluno2 = "Maria";
+        String ra_aluno2 = "654321";
+
+        int nota1_aluno2 = 20;
+        int nota2_aluno2 = 30;
+        int nota3_aluno2 = 40;
+
+        int tota_notas_aluno2 = nota1_aluno2 + nota2_aluno2 + nota3_aluno2;
+
+        System.out.println("Aluno: " + nome_aluno1);
+        System.out.println("RA: " + ra_aluno1);
+        System.out.println("Nota 1: " + nota1_aluno1);
+        System.out.println("Nota 2: " + nota2_aluno1);
+        System.out.println("Nota 3: " + nota3_aluno1);
+        System.out.println("Total de notas: " + tota_notas_aluno1);
+
+        System.out.println("Aluno: " + nome_aluno2);
+        System.out.println("RA: " + ra_aluno2);
+        System.out.println("Nota 1: " + nota1_aluno2);
+        System.out.println("Nota 2: " + nota2_aluno2);
+        System.out.println("Nota 3: " + nota3_aluno2);
+        System.out.println("Total de notas: " + tota_notas_aluno2);
+
+
+    }
+}
