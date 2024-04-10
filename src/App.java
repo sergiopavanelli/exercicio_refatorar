dbc16af9a59fd19c781a7b5034b6ac88dde28ffa
Sérgio Pavanelli <161322303+sergiopavanelli@users.noreply.github.com>
2024-04-10 10:33:57 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
         Aluno aluno1 = new Aluno("João","123456",25,30,35); 
         Aluno aluno2 = new Aluno("Maria","654321",20,30,40);   
         
-        Pessoa pessoa1 =new Pessoa("Joao","123456");
+        Pessoa pessoa1 = new Pessoa("Joao","123456");
         Pessoa pessoa2 = new Pessoa("Maria", "654321");
 
         System.out.println(pessoa1.falar());
